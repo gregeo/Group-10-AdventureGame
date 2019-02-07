5cc46687a59c6974e1690721a2463b1856b6596d
matthewJarrams <47400145+matthewJarrams@users.noreply.github.com>
2019-02-07 11:26:39 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld
 		System.out.println("Please enter your name: ");
 		String name = scan.nextLine();
 		System.out.println("Hello " + name);
+		System.out.println("Hello github");
 		
 		
 	}
