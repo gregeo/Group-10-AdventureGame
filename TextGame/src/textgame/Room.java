58b39579c97a60eeaf6b8f158b67ab6ca0adc7d9
George Smith <smith0064@gmail.com>
2019-02-19 11:14:26 -0700

    room class

--- TextGame/src/textgame/Room.java
+++ TextGame/src/textgame/Room.java
@@ -7,8 +7,65 @@ package textgame;
 
 /**
  *
+ * a calss that will run a room and its dissision making 
+ * 
  * @author George Smith
  */
 public class Room {
+    private boolean[] isDoor;//denoted nesw
+    Enemy enemyRef;
+    String item;
     
+    
+    /**
+     * 
+     * @param isDoor
+     * @param enemyRef
+     * @param item 
+     */
+    public Room(boolean[] isDoor, Enemy enemyRef, String item){
+        if(isDoor.length == 4){
+            this.isDoor = isDoor;
+        }else{
+            System.out.println("isDoor is the wrong sise :(  "+ isDoor.length);
+        }
+        this.enemyRef = enemyRef;
+        this.item = item;
+    }
+    /**
+     * 
+     * @param isDoor
+     * @param item 
+     */
+        public Room(boolean[] isDoor, String item){
+        if(isDoor.length == 4){
+            this.isDoor = isDoor;
+        }else{
+            System.out.println("isDoor is the wrong sise :(  "+ isDoor.length);
+        }
+        this.item = item;
+    }
+        /**
+         * 
+         * @param isDoor
+         * @param enemyRef 
+         */
+        public Room(boolean[] isDoor, Enemy enemyRef){
+        if(isDoor.length == 4){
+            this.isDoor = isDoor;
+        }else{
+            System.out.println("isDoor is the wrong sise :(  "+ isDoor.length);
+        }
+        this.item = item;
+    }/**
+     * 
+     * @param isDoor 
+     */
+        public Room(boolean[] isDoor){
+        if(isDoor.length == 4){
+            this.isDoor = isDoor;
+        }else{
+            System.out.println("isDoor is the wrong sise :(  "+ isDoor.length);
+        }
+    }
 }
