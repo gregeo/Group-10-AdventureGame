fddc36d68b1518b60d32b52511cc71cd0de6bced
TheMrKit <jarhil123@gmail.com>
2019-02-19 11:37:56 -0700

    Updated Item Class

--- TextGame/src/textgame/Item.java
+++ TextGame/src/textgame/Item.java
@@ -1,8 +1,25 @@
-package demo1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textgame;
+
+/**
+ *a class to handle spesific enemys
+ *
+ * 
+ * @author Jarred Hilao
+ */
 
 public class Item {
     boolean toBeFound;
     String showName;
+    int type;
+    /*
+        type 1: health potion
+        type 2: seeing orb
+    */
 
     public void use() {
         
