8159a437bfc6f275710d2a66d6a2f8a85e47914d
TheMrKit <jarhil123@gmail.com>
2019-02-21 16:01:58 -0700

    Updated Versions

--- TextGame/src/textgame/EnemyHandler.java
+++ TextGame/src/textgame/EnemyHandler.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package textgame;
+import java.util.ArrayList;
 
+import demo1.Enemy;
 /**
  *
  * a class to handle a ArrayList<Enemy>
@@ -12,5 +14,37 @@ package textgame;
  * @author George Smith
  */
 public class EnemyHandler {
+    private ArrayList<Enemy> enemyList = new ArrayList<Enemy>();
+
+    public EnemyHandler()
+    {
+        this(new ArrayList<Enemy>());
+    }
+
+    public EnemyHandler(ArrayList<Enemy> enemyList)
+    {
+        this.enemyList = enemyList;
+    }
+
+    public EnemyHandler(EnemyHandler enemyHandlerToCopy)
+    {
+        this.enemyList = enemyHandlerToCopy.enemyList;
+    }
+
+    public void removeEnemy(int index)
+    {
+        enemyList.remove(index);
+    }
+
+    public void removeEnemy(Enemy enemy)
+    {
+        enemyList.remove(enemy);
+    }
+
+    public addEnemy(Enemy enemy)
+    {
+        enemyList.add(enemy);
+    }
+
     
 }
