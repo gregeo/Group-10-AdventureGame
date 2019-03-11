db546037d95130e7f2d8d5ab1ea37f0c73c6e0b8
TheMrKit <jarhil123@gmail.com>
2019-03-11 08:31:37 -0600

    Rough GUI

---
+++ TextGame/src/textgame/Gamegui.java
@@ -0,0 +1,57 @@
+package textgame;
+
+import java.util.ArrayList;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBuilder;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Gamegui extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Canvas canvas = new Canvas(500, 500);
+        BorderPane posRoot = new BorderPane();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        VBox doableActions = new VBox();
+
+        //While (GET TEXT GAME RUN VALUE) {
+            Room refRoom = new Room();
+            ArrayList<String> refActions = refRoom.getDoableActions();
+            ArrayList<Button> actionArray = new ArrayList();
+
+            for (int i = 0; i < refActions.size(); i++) {
+                actionArray.add(new Button(refActions.get(i)));
+            }
+            Button move = new Button("MOVE");
+            Button attack = new Button("ATTACK");
+            doableActions.getChildren().add(move);
+            doableActions.getChildren().add(attack);
+
+            posRoot.setLeft(doableActions);
+
+            gc.strokeRect(380, 10, 115, 115);
+            gc.strokeRect(390, 20, 25, 25);
+            gc.strokeRect(420, 20, 25, 25);
+            gc.strokeRect(420, 50, 25, 25);
+            gc.strokeLine(100, 0, 100, 500);
+
+            root.getChildren().add(posRoot);
+            root.getChildren().add(canvas);
+
+            Scene scene = new Scene(root, 500, 500);
+            primaryStage.setScene(scene);
+
+            primaryStage.setTitle("Dungeon Hero");
+            primaryStage.show();
+        //}
+    }
+}
